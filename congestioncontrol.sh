149b7aab7574b16bf0691c9957f712d9f07462f2
fennih <fenni.v@gmail.com>
2019-06-27 04:43:13 +0100

    Add files via upload

--- congestioncontrol.sh
+++ congestioncontrol.sh
@@ -8,6 +8,9 @@ echo "Insert number of sealers nodes"
 read sealers
 echo "Insert total number of nodes"
 read nodes
+
+threshold=160
+
 for((c=1;c<=$sealers;c++))
 do
         awk /'mined/ {print $8}' node$c.txt | cut -c 6- >> hashnode$c.txt
@@ -15,7 +18,6 @@ done
 cat hashnode*.txt > totalhash.txt
 rm hashnode*.txt
 tot=`wc -l < totalhash.txt`
-echo $tot
 for((x=1;x<=$tot;x++))
 do
 	for ((y=1;y<=$nodes;y++))
@@ -28,9 +30,9 @@ do
 done
 sed -i '/^$/d' temp/*.txt
 rm temp/filenode*.txt
-for((p=1;p<=$tot;p++))
+for((u=1;u<=$tot;u++))
 do
-	cut -d ']' -f 1 temp/finalfilenode$p.txt > temp/final$p.txt
+	cut -d ']' -f 1 temp/finalfilenode$u.txt > temp/final$u.txt
 done
 rm temp/finalfile*.txt
 for((p=1;p<=$tot;p++))
@@ -39,10 +41,13 @@ do
 	max=`sort temp/final$p.txt | tail -1`
 	difference="$(( $(timestamp "$max") - $(timestamp "$min") ))"
 	totaldiff=`echo $difference / 1000000 | bc -l`
-	echo $totaldiff
-	echo $totaldiff >> timepropagation.txt
+	try=`echo ${totaldiff%\.*}`
+	if (("$try" <= "$threshold")); then echo $totaldiff >> timepropagation.txt
+	else
+	echo $min "is min and "$max "is max"
+	fi
 done
 
 average_prop=`awk '{s+=$1} END {print s/NR}' timepropagation.txt`
 echo "The propagation time on average is " $average_prop "ms"
-echo $average_prop >> /home/ubuntu/statistiche/$nodes/average_prop.txt
+echo $average_prop >> /home/ubuntu/prova/cc_prop.txt
